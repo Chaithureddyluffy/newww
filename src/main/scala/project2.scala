72c61d368bc346f36a75ef48865314bb8846745e
chaithu <poolachaithanyareddy@gmail.com>
2025-01-14 23:32:17 +0530

    this is my secondcommit

--- src/main/scala/project2.scala
+++ src/main/scala/project2.scala
@@ -1,6 +1,282 @@
 object project2 {
   def main(args:Array[String]): Unit={
-    print("hello")
+// 1---   println("Enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if (x % 2 == 0){
+//      println("even number")
+//    }
+//    else{
+//      println("odd number")
+//    }
+    //---------------------------------------------------------------------
+    //2nd problem
+//    println("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if( x < -10 || x > 10){
+//      println("greater than 10")
+//    }
+//    else{
+//      println("lees than 10")
+//    }
+    // 3rd problem
+//    println("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//
+//    if(x%3==0){
+//      print("Divisble by 3")
+//    }
+//    else{
+//      print("Not Divisible by 3")
+//    }
+    //4th problem
+//     println("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if(x%12==0){
+//      println("divisible by 4 and 6")
+//    }
+//    else{
+//      println(" Not divisible by 4 and 6")
+//    }
+    //5th problem
+//    println("Enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if(x>18){
+//      println("Eligible for voting")
+//    }
+//    else{
+//      println("Not eligible for voting")
+//    }
+//6th problem
+//    println("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if (x>1&&x<10 || x>20 && x<30) {
+//      println(s"The number $x is in the range (1, 10) or (20, 30).")
+//    } else {
+//      println(s"The number $x is not in the range (1, 10) or (20, 30).")
+//    }
+    //7 th problem
+//     print("enter the number")
+//    val x=scala.io.StdIn.readInt()
+//    if (x<0 && x%2!=0){
+//      print("the number is negative and odd number")
+//    }
+//    else if(x<0 && x%2==0) {
+//      print("Number is negative and even number")
+//    }
+//      else if(x>0 && x%2==0){
+//        print("num is postive and even number")
+
+//    }
+//8 th question
+//    print("Enter the age")
+//    var x=scala.io.StdIn.readInt()
+//    if(x<25){
+//      print("eligible for student discount")
+//    }
+//    else if(x>60){
+//      print("Eligible for the senior citizen discount")
+//    }
+//    else{
+//      print("not eligible for anything")
+//    }
+    //9 th question
+//    print("enter the number")
+//    val x=scala.io.StdIn.readInt()
+//    if(x%5==0 && x%7==0) {
+//      print("divibisible by 5 and 7")
+//    }
+//    else{
+//      print("not divisible by both 5 and 7")
+//    }
+// 10 th problem
+//    print("enter the number")
+//    val x=scala.io.StdIn.readInt()
+//    if(x%2==0 && x<0){
+//      print(" the number is divisible by 2  and is negative")
+//    }
+//    else if(x%2==0 && x>0){
+//      print("the number is even but postive")
+//    }
+//    else{
+//      print("not divisible by 2")
+//    }
+//11
+//    println("Enter the number:")
+//    val x = scala.io.StdIn.readInt()
+//    var isPrime = true
+//
+//    if (x <= 1) {
+//      isPrime = false
+//    } else {
+//      for (i <- 2 until x) {
+//        if (x % i == 0) {
+//          isPrime = false
+//        }
+//      }
+//    }
+//    if (isPrime) {
+//      println("Number is prime")
+//    } else {
+//      println("Number is not prime")
+//    }
+//12
+//    print("enter the number")
+//    var purchase=scala.io.StdIn.readInt()
+//    if(purchase>150){
+//      print("Eligible for Discount")
+//    }
+//    else if(purchase>100){
+//      print("eligible for free shipping")
+//    }
+//    else{
+//      print("not eligible")
+//    }
+    //13
+//    print("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if (x%3==0 || x%8==0){
+//      print("the number is divisble")
+//    }
+//    else{
+//      print("not eligible")
+//    }
+    //14
+//    print("enter the number")
+//    var x= scala.io.StdIn.readInt()
+//    if(x<0 && x%2==0){
+//      print("it is a negative and even number")
+//    }
+//    else if(x>0 && x%2!=0)
+//      print("postive number and Odd number")
+//    else if(x>0&& x%2==0){
+//      print("postive but even number")
+//    }
+//    else if(x<0 && x%2!=0){
+//      print("negative and odd number")
+//    }
+//    else{
+//      print("none")
+//    }
+//15 th problem
+//    print("enter the age")
+//    val x=scala.io.StdIn.readInt()
+//    if(x<13){
+//      print("children")
+//    }
+//    else if(x>15 && x<19){
+//      print("teenager")
+//    }
+//    else{
+//      print("Adult")
+//    }
+    //16 th
+//    var x=25
+//    if(x%2==0 || x%5==0){
+//      print("yes its  either 2 or 5 divisible")
+//    }
+//    else{
+//      print("its not divisble")
+//    }
+    //17
+
+//    print("Enter the age")
+//        var x=scala.io.StdIn.readInt()
+//        if(x<25){
+//          print("eligible for student discount")
+//        }
+//        else if(x>60){
+//          print("Eligible for the senior citizen discount")
+//        }
+//        else{
+//          print("not eligible for anything")
+//        }
+    //19
+
+//    print("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    if(x%3==0 && x%7==0){
+//      print(" yes it is divsible by both 3 and 7")
+//    }
+//    else if(x%3==0 || x%7==0) {
+//      print("its is divisble by either 3 or 7")
+//    }
+//    else{
+//      print(" its is not divisble by 3 and 7")
+//    }
+//20
+//    print("enter the number")
+//        var x=scala.io.StdIn.readInt()
+//        if(x%5==0 && x%9==0){
+//          print("yes it is divisbsle by both 5 and 9")
+//        }
+//        else if(x%5==0|| x%9==0){
+//          print("yes its divisble by either 5 or 9")
+//        }
+//    else{
+//      print(" its not divisible by either")
+//    }
+    //21
+//    print("enter the number")
+//    val x=scala.io.StdIn.readInt()
+//    if(x%2!=0 && x%4!=0){
+//      print(" number is odd and not divisble by 4")
+//    }
+//    else{
+//      print(" even number")
+//    }
+//22
+//    val x=15
+//    if(x%3==0 && x%5==0){
+//      print(" yes it divible by 3 and 5")
+//    }
+//    else{
+//      print(" not divisible")
+//    }
+
+//23
+//           print("Enter the purchase")
+//            var x=scala.io.StdIn.readInt()
+//            print("checking for loyality123")
+//             var y=scala.io.StdIn.readBoolean()
+//            if(x>200){
+//              print("eligible for  discount")
+//            }
+//            else if(y){
+//              print("Qualifes for Membership benefits")
+//            }
+//            else{
+//              print("not eligible for anything")
+//            }
+   // 26
+
+//    print("Enter the age")
+//                var x=scala.io.StdIn.readInt()
+//                print("checking for newcoustomer")
+//                 var y=scala.io.StdIn.readBoolean()
+//                if(x>65){
+//                  print("eligible for  discount")
+//                }
+//                else if(y){
+//                  print("no benefits")
+//                }
+//                else{
+//                  print("not eligible for anything")
+//                }
+    // 24 and 27 and 28 and 29 are Repeat So skipping
+30
+//    print("Enter the age")
+//    val age=scala.io.StdIn.readInt()
+//    print("check for free trail")
+//    val freetrail=scala.io.StdIn.readBoolean()
+//    if(age<25){
+//      print("eligible for Student Discount")
+//    }
+//    else if(freetrail){
+//      print("eligible for free trail")
+//    }
+//    else{
+//      print("eligible for nothing")
+//    }
   }
 
 }
