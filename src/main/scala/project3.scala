baafa11b011dd7d2e78118eebc3a549c57a850ec
chaithu <poolachaithanyareddy@gmail.com>
2025-01-15 17:15:17 +0530

    this is fouth commit

---
+++ src/main/scala/project3.scala
@@ -0,0 +1,110 @@
+import org.apache.hadoop.fs.shell.Count
+
+object project3 {
+  def main(array: Array[String]):Unit= {
+    ////    print("helloworld")
+    //->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 1st problem
+    //    print("enter the number")
+    //    var x=scala.io.StdIn.readInt()
+    //    for(i <-1 to x){
+    //      println(i)
+    //    }
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2nd problem
+    //    var x=2
+    //    while(x<12){
+    //      println(x)
+    //      x=x+2
+    //    }
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  3rdproblem
+    //    print("Enter the number")
+    //    var sum=0
+    //    var x=scala.io.StdIn.readInt()
+    //    for(i <-1 to x ){
+    //      sum=sum+i
+    //    }
+    //    println(sum)
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 4th problem
+    //    print("enter the number")
+    //    var x=scala.io.StdIn.readInt()
+    //    for(i <-1 to x){
+    //      println(i*i)
+    //    }
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>6th probelm
+    //    print("enter the number")
+    //        var x=scala.io.StdIn.readInt()
+    //        for(i <-1 to x){
+    //          println(i*3)
+    //        }
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>7th problem
+    //    var x=1
+    //    while(x<15){
+    //      println(x)
+    //      x=x+2
+    //    }
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    //    print("enter the number")
+    //    var x=scala.io.StdIn.readInt()
+    //    var fact=1
+    //    for (i <-1 to x){
+    //      fact*=i
+    //    }
+    //    print(fact)
+//_______________________________________________________________________________________
+    //    print("enter the string")
+    //    var x=scala.io.StdIn.readLine()
+    //    for(i <- x.length -1 to 0 by -1){
+    //      print(x(i))
+    //    }
+    //___________________________________________________________________________________________________
+    //    var a=Array(1,2,3,4,5,6)
+    //    var b=new Array[Int](a.length)
+    //    for(i <- a.indices){
+    //      b(i)= a(a.length-1-i)
+    //    }
+    //    println(b.mkString(" "))
+    //______________________________________________________________________________________________________
+//    var sum=0
+//   var x=1
+//    while(x<=20){
+//      if(x%2==0){
+//        sum=sum+x
+//      }
+//      x=x+1
+//    }
+//    println(sum)
+    //________________________________________________________________________________
+//    print("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    for(i <-1 to x){
+//      for (j <-1 to i){
+//        print('*')
+//      }
+//      println()
+//    }
+    //_____________________________________________________________
+//    var x=1
+//    while (x<=3){
+//      println(x * x * x)
+//      x=x+1
+//    }
+//____________________________________________________________-
+//        println("Enter a string:")
+//        val input = scala.io.StdIn.readLine()
+//        val vowels = Set('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+//
+//        val vowelCount = input.count(char => vowels.contains(char))
+//
+//        println(vowelCount)
+
+//___________________________________________________
+//    print("enter the number")
+//    val x= scala.io.StdIn.readInt()
+//    for(i <-1 to x){
+//      for(j <-1 to i){
+//        print(i)
+//      }
+//      println()
+//    }
+
+    }
+}
