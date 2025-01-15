37d7e1888f2ec256251d1ef271e89bd337798b3f
chaithu <poolachaithanyareddy@gmail.com>
2025-01-15 17:17:25 +0530

    5 th commit

---
+++ src/main/scala/Project4.scala
@@ -0,0 +1,73 @@
+object Project4 {
+  def main(arr:Array[String]): Unit = {
+//    print("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    for(i <-1 to x){
+//      for (j <-1 to i){
+//        print('*')
+//      }
+//      println()
+//    }
+//    print("enter the number")
+//    var x=scala.io.StdIn.readInt()
+//    for(i<-1 to x){
+//      for(j<-1 to x){
+//        print('*')
+//      }
+//      println()
+//    }
+//var x=scala.io.StdIn.readInt()
+//        for(i <-1 to x){
+//          for (j <-1 to i){
+//            print('*')
+//          }
+//          println()
+//        }
+
+
+//    for(i <-5 to 1 by -1){
+//      for (j <-i to 1 by -1){
+//        print('*')
+//      }
+//      println()
+//    }
+//    for (i <- 1 to 4) {
+//      for (j <- 1 to 6) {
+//        if (i == 1 || i == 4 || j == 1 || j == 6) {
+//          print("*")
+//        } else {
+//          print(" ")
+//        }
+//      }
+//      println()
+//    }
+//    for(i<-1 to 5){
+//      for (j <-1 to i){
+//        print("*_")
+//      }
+//      println()
+//    }
+
+//    val x=List(1,3,5,7,9,7,5,3,1)
+//    for(i<-x){
+//      println("*" * i)
+//    }
+//    val x=List(7,5,3,1,1,3,5,7)
+//    for(i<-x){
+//      println("*" * i)
+//    }
+
+//    for (i <- 1 to 9 by 2) {
+//
+//      for (_ <- 1 to (7 - i) / 2) {
+//        print(" ")
+//      }
+//      for (_ <- 1 to i) {
+//        print("*")
+//      }
+//      println()
+  //}
+  }
+
+
+}
